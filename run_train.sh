349f3488c026e6dd8c58eb9b80960bf54db21906
Anton Ehrmanntraut <anton.ehrmanntraut@uni-wuerzburg.de>
2025-01-28 18:38:13 +0100

    single node run

--- run_train.sh
+++ run_train.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #SBATCH -J modernbert
-#SBATCH -c 8
-#SBATCH --mem=16G
+#SBATCH -c 64
+#SBATCH --mem=128G
 #SBATCH -p h100
-#SBATCH --gres=gpu:1             
-#SBATCH --time 1:00:00
+#SBATCH --gres=gpu:8             
+#SBATCH --time 1-00:00:00
+#SBATCH --mail-type ALL
+#SBATCH --mail-user anton.ehrmanntraut@uni-wuerzburg.de
 
 cd ~/ModernBERT_original
 source venv/bin/activate
 
+export PYTHONUNBUFFERED=1
+export NCCL_DEBUG=INFO
 export JOBNAME=modernbert_base_${SLURM_JOB_ID}
 
-srun /bin/bash -c "export NODE_RANK=\$SLURM_NODEID; composer main.py yamls/main/modernbert-base.yaml run_name=$JOBNAME 2>&1 | awk -v node_rank=\$(cat /etc/hostname) '{ print node_rank, strftime(\"%c: \"), \$0; fflush(); }'"
+srun /bin/bash -c "export NODE_RANK=\$SLURM_NODEID; composer -n 8 main.py yamls/main/modernbert-base.yaml run_name=$JOBNAME 2>&1 | awk -v node_rank=\$(cat /etc/hostname) '{ print node_rank, strftime(\"%c: \"), \$0; fflush(); }'"
